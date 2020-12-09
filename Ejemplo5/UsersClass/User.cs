d885323f78a6b53eca5aba42298f30d399322a6e
cury009 <63730861+cury009@users.noreply.github.com>
2020-12-09 21:48:37 +0100

    Agregar archivos de proyecto.

---
+++ Ejemplo5/UsersClass/User.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo5.UsersClass
+{
+    public class User: ICloneable
+    {
+        public String nombre { set; get; }
+        public String telefono { set; get; }
+        public DateTime fechaAlta { set; get; }
+
+        public User(string nombre, string telefono, DateTime fechaAlta)
+        {
+            this.nombre = nombre;
+            this.telefono = telefono;
+            this.fechaAlta = fechaAlta;
+        }
+        public User()
+        {
+            this.nombre = "";
+            this.telefono = "";
+            this.fechaAlta = DateTime.Now;
+        }
+
+        public override string ToString()
+        {
+            return nombre;
+        }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+           
+        }
+    }
+}
---
+++ Ejemplo5/UsersClass/UsersHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo5.UsersClass
+{
+    public class UsersHandler
+    {
+        public ObservableCollection<User> userList { get; set; }
+
+        public UsersHandler()
+        {
+            this.userList = new ObservableCollection<User>();
+        }
+
+        public void AddUser(User user)
+        {
+            userList.Add(user);
+        }
+        
+        public void ModifyUser(User user, int pos)
+        {
+            userList[pos] = user;
+        }
+        public void RemoveUser(int pos)
+        {
+            userList.RemoveAt(pos);
+        }
+
+    }
+}
